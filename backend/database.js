bd90d3ec944e8564252ca32b1809029ff24776fe
sirajudeenhl <sirajudeenhl@gmail.com>
2025-08-01 17:50:47 +0530

    addstudent function modified 2nd

--- backend/database.js
+++ backend/database.js
@@ -120,11 +120,11 @@ const readStudentInfo = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, hometown) => {
-    const sql = `INSERT INTO student(id, name, age, hometown) values(?, ?, ?, ?)`
+const addStudent = async ( name, age, hometown, id) => {
+    const sql = `INSERT INTO student(name, age, hometown,id) values(?, ?, ?, ?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql[id, name, age, hometown])
+            .raw(sql[ name, age, hometown, id])
             .then((data) => {
                 resolve(data);
             })
